cfff0f44c60009a45eaca0bcffecb0c1e504a982
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-22 14:57:01 -0400

    Modify index.html and script.js
    Modified index.html to add a div with class container. This will hold our 16x16 div grid.
    Modified script.js to add a for loop that appends 16 divs to our container.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Our container for all of our divs
+const gridContainer = document.querySelector(".container");
 
+function handleDivGridSetup() {
+    // divCounter should be deleted once the grid is set up properly. This is just to display there are 16x16 divs
+    let divCounter = 0;
+
+    for (let index = 0; index < 16; index++) {
+        const newDivForGrid = document.createElement("div");
+        newDivForGrid.innerText = "Div " + divCounter;
+        divCounter++;
+        gridContainer.appendChild(newDivForGrid);;
+    }
+}
+
+handleDivGridSetup();
