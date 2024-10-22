9dd1189ce0e76bb39b437b58c6cdeb7118d42bb2
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-22 18:07:09 -0400

    Modify HTML, CSS, and JS files
    Modifies index.html slightly (I forgor).
    Modified script.js to add functionality so that a user can make a new grid, added black as the initial color, made the random colors be controlled by a button, and added a function to erase the board.
    Modified script.js slightly.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Our container for all of our divs
 const gridContainer = document.querySelector(".container");
 
-function handleDivGridSetup() {
-    // divCounter should be deleted once the grid is set up properly. This is just to display there are 16x16 divs
-    let divCounter = 0;
+// Buttons
+const makeNewGridButton = document.querySelector(".new-grid-button");
+const randomColorButton = document.querySelector(".random-color-button");
 
+function handleDivGridSetup() {
     for (let index = 0; index < 16; index++) {
         // Rows
         const rowContainer = document.createElement("div");
@@ -14,7 +15,6 @@ function handleDivGridSetup() {
             // Columns
             const newDivForGrid = document.createElement("div");
             newDivForGrid.classList.add("inner-div");
-            divCounter++;
             rowContainer.appendChild(newDivForGrid);
         }
     }
@@ -29,14 +29,78 @@ function generateRandomColor(element) {
     element.style.backgroundColor = color;
 }
 
-// This is all of our divs as a NodeList
-const innerDivs = document.querySelectorAll(".inner-div");
+// Sets the initial color to black
+function generateInitialColor(element) {
+    let color = "black";
+    element.style.backgroundColor = color;
+}
 
-// Looping through the divs and adding event listeners
-innerDivs.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        generateRandomColor(div);
+// This is all of our divs as a NodeList
+let innerDivs = document.querySelectorAll(".inner-div");
+
+// Looping through the divs and adding event listeners which adds random colors to the divs background
+function setColorToRandomColor(nodes) {
+    nodes.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            generateRandomColor(div);
+        });
     });
+}
+
+function setColorToInitialColor(nodes) {
+    nodes.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            generateInitialColor(div);
+        });
+    });
+}
+
+// Sets all div background colors to white
+function handleEraseBoard() {
+    innerDivs.forEach((div) => {
+        div.style.backgroundColor = 'white';
+    });
+}
+
+// Removes current grid and makes a new one based on input
+function createNewGrid(verticalAxis, horizontalAxis) {
+    // Remove current grid
+    gridContainer.innerHTML = "";
+    // Make axes numbers
+    Number(verticalAxis);
+    Number(horizontalAxis);
+    for (let index = 0; index < horizontalAxis; index++) {
+        // Rows
+        const rowContainer = document.createElement("div");
+        rowContainer.classList.add("row-container");
+        gridContainer.appendChild(rowContainer);
+        for (let index = 0; index < verticalAxis; index++) {
+            // Columns
+            const newDivForGrid = document.createElement("div");
+            newDivForGrid.classList.add("inner-div");
+            rowContainer.appendChild(newDivForGrid);
+        }
+    }
+}
+
+
+
+makeNewGridButton.addEventListener('click', () => {
+    let vertical = prompt("Please enter the number of blocks for the vertical axis:", "16");
+    let horizontal = prompt("Please enter the number of blocks for the horizontal axis:", "16");
+    if (vertical <= 100 && horizontal <= 100) {
+        createNewGrid(vertical, horizontal);
+        innerDivs = document.querySelectorAll(".inner-div");
+        setColorToInitialColor(innerDivs);
+    } else {
+        alert("Axes cannot be over 100!")
+    }
 });
 
+// Event handler for clicking random color button
+randomColorButton.addEventListener('click', () => {
+    handleEraseBoard();
+    setColorToRandomColor(innerDivs);
+})
 
+setColorToInitialColor(innerDivs);
